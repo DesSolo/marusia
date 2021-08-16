917142d6ed3b5b2ce13c65ed18ad48bf4a858d99
DesSolo <dessolo@mail.ru>
2021-08-17 00:18:39 +0300

    add middlewares

--- router.go
+++ router.go
@@ -7,11 +7,12 @@ import (
 )
 
 // HandlerFunc ...
-type HandlerFunc func(*Request, *Response, context.Context)
+type HandlerFunc func(req *Request, resp *Response, ctx context.Context)
 
 // DialogRouter ...
 type DialogRouter struct {
 	router         map[string]HandlerFunc
+	middlewares    []func(HandlerFunc) HandlerFunc
 	defaultHandler HandlerFunc
 	ignoreCase     bool
 }
@@ -39,11 +40,16 @@ func (r *DialogRouter) RegisterDefault(hf HandlerFunc) {
 	r.defaultHandler = hf
 }
 
+func (r *DialogRouter) Use(f func(next HandlerFunc) HandlerFunc) {
+	r.middlewares = append(r.middlewares, f)
+}
+
 // Select select dialog function by token name
 func (r *DialogRouter) Select(token string) (HandlerFunc, error) {
 	hf, ok := r.router[token]
 	if ok {
-		return hf, nil
+		h := chain(r.middlewares, hf)
+		return h, nil
 	}
 
 	if r.defaultHandler != nil {
@@ -51,4 +57,17 @@ func (r *DialogRouter) Select(token string) (HandlerFunc, error) {
 	}
 
 	return nil, fmt.Errorf("dialog endpoint not found by token: %s", token)
+}
+
+func chain(middlewares []func(HandlerFunc) HandlerFunc, endpoint HandlerFunc) HandlerFunc {
+	if len(middlewares) == 0 {
+		return endpoint
+	}
+
+	h := middlewares[len(middlewares)-1](endpoint)
+	for i := len(middlewares) - 2; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+
+	return h
 }
